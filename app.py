19c452ec678f7a8c52d6ba4a3b02adb2dc22ae93
Shailesh Unde <shaileshunde0@gmail.com>
2026-03-13 14:24:23 +0530

    fixing url issues

--- app.py
+++ app.py
@@ -61,6 +61,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "store.db"))
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+database_url = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "store.db"))
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
 db = SQLAlchemy(app)
 
 from flask_migrate import Migrate
